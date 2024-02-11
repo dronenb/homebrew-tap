488f5267941c44baf896b65adb42833687596468
dronenb <dronenb@users.noreply.github.com>
2024-02-11 01:16:04 -0500

    feat(dvs): add cask

---
+++ Casks/dante-virtual-soundcard.rb
@@ -0,0 +1,22 @@
+cask "dante-virtual-soundcard" do
+  version "4.4.1.3"
+  sha256 "ff2b5384b4905ed479915e8d4f9a1220abc88a379a0e129d2f76c302c8e72a99"
+
+  url "https://audinate-software-updates.sgp1.cdn.digitaloceanspaces.com/DanteVirtualSoundcard/#{version.major}/#{version.major_minor}/macOS/DVS-#{version}_macos.dmg",
+      verified: "audinate-software-updates.sgp1.cdn.digitaloceanspaces.com/"
+  name "Dante Virtual Soundcard"
+  desc "Turns your computer into a Dante-powered workstation"
+  homepage "https://www.audinate.com/products/software/dante-virtual-soundcard"
+
+  auto_updates true
+
+  app "Uninstall DVS.app"
+  pkg "DanteVirtualSoundcard.pkg"
+
+  uninstall_preflight do
+    system_command  "#{appdir}/Uninstall DVS.app/Contents/Resources/uninstall.sh",
+                    sudo: true
+  end
+
+  uninstall delete: "/Applications/Uninstall DVS.app"
+end
