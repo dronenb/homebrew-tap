3e12285da9755a826626bc5c82755bcb703e2ea3
dronenb <dronenb@users.noreply.github.com>
2024-02-12 20:44:36 -0500

    chore(vdhcoapp): add mojave and older support

--- Casks/video-downloadhelper-companion.rb
+++ Casks/video-downloadhelper-companion.rb
@@ -2,10 +2,19 @@ cask "video-downloadhelper-companion" do
   arch arm: "arm64", intel: "x86_64"
 
   version "2.0.17"
-  sha256  arm:   "e67e33c46fd6e8a6c08647ee16c4f77746a234bf06a3cff9d2fa771f666a6d57",
-          intel: "e043cb2e255836ad749ab626ef34d186cb9df66741728af9dde67a8c7308dffb"
 
-  url "https://github.com/aclap-dev/vdhcoapp/releases/download/v#{version}/vdhcoapp-mac-#{arch}-installer.pkg"
+  on_mojave :or_older do
+    sha256 "1d826c8456424445309dac74054f0d954dab2eb089c75ef217de0b08c32dd5c5"
+
+    url "https://github.com/aclap-dev/vdhcoapp/releases/download/v#{version}/vdhcoapp-mac13-#{arch}-installer.pkg"
+  end
+  on_catalina :or_newer do
+    sha256  arm:   "e67e33c46fd6e8a6c08647ee16c4f77746a234bf06a3cff9d2fa771f666a6d57",
+            intel: "e043cb2e255836ad749ab626ef34d186cb9df66741728af9dde67a8c7308dffb"
+
+    url "https://github.com/aclap-dev/vdhcoapp/releases/download/v#{version}/vdhcoapp-mac-#{arch}-installer.pkg"
+  end
+
   name "Video DownloadHelper Companion"
   desc "Companion application for Video DownloadHelper browser add-on"
   homepage "https://github.com/aclap-dev/vdhcoapp"
