2d0f9a7529e6ce06258c4dda0393e3f6066f2e08
Ben Dronen <dronenb@users.noreply.github.com>
2024-10-28 15:35:28 -0500

    feat: add some stanzas to cask
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

--- Casks/shure-update-utility.rb
+++ Casks/shure-update-utility.rb
@@ -7,7 +7,19 @@ cask "shure-update-utility" do
   desc "Helps you update compatible Shure products"
   homepage "https://www.shure.com/en-US/products/software/shure_update_utility"
 
-  auto_updates true
+  auto_updates false
 
   pkg "Shure-Update-Utility-macOS-#{version}.pkg"
+
+  uninstall quit:    "com.shure.suu",
+            pkgutil: "com.shure.suu"
+
+  zap trash: [
+    "~/Library/Application Support/Shure/Shure Update Utility",
+    "~/Library/Saved Application State/com.shure.savedState",
+  ]
+
+  caveats do
+    requires_rosetta
+  end
 end
