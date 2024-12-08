310e2b924462ef958d0b8c27d8ba37a2df3f8126
dronenb <dronenb@users.noreply.github.com>
2024-12-08 18:39:11 -0500

    chore(nmstatectl): bump to 2.2.39 (#48)
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

--- Formula/nmstatectl.rb
+++ Formula/nmstatectl.rb
@@ -2,22 +2,22 @@ class Nmstatectl < Formula
   desc "Command-line tool that manages host networking settings in a declarative manner"
   homepage "https://github.com/nmstate/nmstate"
   url "https://github.com/nmstate/nmstate/releases"
-  version "2.2.38"
+  version "2.2.39"
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/nmstate/nmstate/releases/download/v#{version}/nmstatectl-macos-x64.zip"
-      sha256 "c0c94c4c7b4d3dbd7059c8d0da7d93812a1fcbc884214920bdd5162f277a3a71"
+      sha256 "4b8e9a9988de337053987f7d5f3024e61f0498653742cfa30a62324eacca0799"
     end
     if Hardware::CPU.arm?
       url "https://github.com/nmstate/nmstate/releases/download/v#{version}/nmstatectl-macos-aarch64.zip"
-      sha256 "b6b06fa5b1cb6e776277ce558b7a9b8efa1c497adf716336d066f95954920d62"
+      sha256 "c6e5210b62eb8ed005712ded9318f85a6f5df8682e30ca03e73631b9f61371b1"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/nmstate/nmstate/releases/download/v#{version}/nmstatectl-linux-x64.zip"
-      sha256 "c6a3ed7f0a2d284b78cada49e2c3abeb2b366511f4c8ee19b2b7377ec2806675"
+      sha256 "8b563db85c145d814f531e1d998dc0db53c4c16d8b7cf73da89308dda82913c6"
     end
   end
 
