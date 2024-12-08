8cd3c412849cc3fa2576c2399ad42f14f7db23f0
Ben Dronen <dronenb@users.noreply.github.com>
2024-12-08 18:26:20 -0500

    dante-controller 4.14.0.17

--- Casks/dante-controller.rb
+++ Casks/dante-controller.rb
@@ -1,6 +1,6 @@
 cask "dante-controller" do
-  version "4.13.3.2"
-  sha256 "280742df46d8f5661adf8d24a0ee37d0f0d3e0c27d4784e706e339a1f555ba6b"
+  version "4.14.0.17"
+  sha256 "fd8b849db5781eab04089373975a5b3d92f87e54e3b2daa3d3a7d1505cd50039"
 
   url "https://audinate-software-updates.sgp1.cdn.digitaloceanspaces.com/DanteController/#{version.major}/#{version.major_minor}/DanteController-#{version}_macos.dmg",
       verified: "audinate-software-updates.sgp1.cdn.digitaloceanspaces.com/"
