ad70824b2a5d14fa4eb04dbec5f99276abf3ad92
Ben Dronen <dronenb@users.noreply.github.com>
2025-03-09 10:19:14 -0400

    chore: update some casks
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

--- Casks/m7cl-editor.rb
+++ Casks/m7cl-editor.rb
@@ -1,8 +1,8 @@
 cask "m7cl-editor" do
-  version "3.6.0"
-  sha256 "0eef85919d4d29fc6d1e16405f98950f5d0be1acacd6569c64f7da21d926d6d7"
+  version "3.6.1"
+  sha256 "a2c5b7f50411f9af018ca6bca5e76e0f0a9482f267b38cf2c53b0a4e535ff265"
 
-  url "https://usa.yamaha.com/files/download/software/3/1398643/m7clv3_edt#{version.no_dots}_mac.zip"
+  url "https://usa.yamaha.com/files/download/software/5/2320605/m7clv3_edt#{version.no_dots}_mac.zip"
   name "M7CL Editor"
   desc "Provides a complete interface for offline editing and setup of M7CL consoles"
   homepage "https://usa.yamaha.com/products/proaudio/software/m7cl_v3_editor/index.html"
