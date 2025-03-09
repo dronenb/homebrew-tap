721a3a244d539cf62788e13cff23c60c02f65e1f
Ben Dronen <dronenb@users.noreply.github.com>
2025-03-09 15:10:04 -0400

    chore(nmstatectl): convert to build from tarball instead of git as it's preferred by upstream
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

--- Formula/nmstatectl.rb
+++ Formula/nmstatectl.rb
@@ -1,18 +1,14 @@
 class Nmstatectl < Formula
   desc "Command-line tool that manages host networking settings in a declarative manner"
   homepage "https://nmstate.io/"
-  url "https://github.com/nmstate/nmstate.git",
-    tag:      "v2.2.42",
-    revision: "cfec22f532a03aaeeaa5afb459e7f25195c35291"
+  url "https://github.com/nmstate/nmstate/releases/download/v2.2.42/nmstate-2.2.42.tar.gz"
+  sha256 "ca1a1db4df29043cecadb705535c16ac3a19d484c5924b78f2ffd40694891390"
   license "Apache-2.0"
   head "https://github.com/nmstate/nmstate.git", branch: "base"
 
   depends_on "rust" => :build
-  depends_on "rustup" => :build
 
   def install
-    system "rustup", "default", "stable"
-    system "rustup", "update"
     cd "rust/src/cli" do
       system "cargo", "install", "--no-default-features", "--features", "gen_conf", *std_cargo_args if OS.mac?
       system "cargo", "install", *std_cargo_args if OS.linux?
@@ -20,7 +16,6 @@ class Nmstatectl < Formula
   end
 
   test do
-    # Validate install
-    system "#{bin}/nmstatectl", "version"
+    system bin/"nmstatectl", "version"
   end
 end
