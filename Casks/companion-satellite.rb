12a472a08aa5e5038c145e8213adce533f17ed3b
dronenb <dronenb@users.noreply.github.com>
2025-03-09 11:37:05 -0400

    chore(companion): update cask versions (#52)
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

--- Casks/companion-satellite.rb
+++ Casks/companion-satellite.rb
@@ -1,21 +1,23 @@
 cask "companion-satellite" do
-  arch arm: "arm64", intel: "x64"
+  on_macos do
+    arch arm: "arm64", intel: "x64"
 
-  version "337-341e49f"
-  sha256  arm:   "1a98b1c091a9dd8adc603cf3b48ef18d70647b484f398a59a87e5eae3409b652",
-          intel: "18ef9696e955aa826fc96222e44519900352f7691c1daf76141cac3dbd712633"
+    version "459-b31b4b0"
+    sha256  arm:    "baf413c0812eb8d5b61bd2e3b9ebca20690876b7162739c87cdc6372d4829fe4",
+            x86_64: "72b27a96597a8750aa4bc6867ee353dd68b1720cc1176441bfcca973b69e2c70"
 
-  url "https://s3.bitfocus.io/builds/companion-satellite/companion-satellite-#{arch}-#{version}.dmg"
-  name "Companion Satellite"
-  desc "Lets you connect another computer with a Stream Deck attached"
-  homepage "https://bitfocus.io/companion-satellite"
+    url "https://s3.bitfocus.io/builds/companion-satellite/companion-satellite-#{arch}-#{version}.dmg"
+    name "Companion Satellite"
+    desc "Lets you connect another computer with a Stream Deck attached"
+    homepage "https://bitfocus.io/companion-satellite"
 
-  app "Companion Satellite.app"
+    app "Companion Satellite.app"
 
-  uninstall quit: "remote.companion.bitfocus.no"
+    uninstall quit: "remote.companion.bitfocus.no"
 
-  zap trash: [
-    "~/Library/Application Support/companion-satellite",
-    "~/Library/Preferences/remote.companion.bitfocus.no.plist",
-  ]
+    zap trash: [
+      "~/Library/Application Support/companion-satellite",
+      "~/Library/Preferences/remote.companion.bitfocus.no.plist",
+    ]
+  end
 end
--- Casks/companion.rb
+++ Casks/companion.rb
@@ -1,27 +1,29 @@
 cask "companion" do
-  arch arm: "arm64", intel: "x64"
+  on_macos do
+    arch arm: "arm64", intel: "x64"
 
-  version "3.3.1+7001-stable-ee7c3daa"
-  sha256  arm:   "92529228103923ea81226b528e3f2fc18049cbaa43b4e4f5366e72ceaae46d55",
-          intel: "28ef0bc37e073ea9d5e3e06b705a6efbac27a2ebfb65aaee83722ec097139e60"
+    version "3.5.2+7764-stable-fe36af46"
+    sha256  arm:    "802392d1eb8d7a63e8f9b60d74501e18fad077e326919072c3af4a2b76059af3",
+            x86_64: "564c0eb2b0e6a59e065c2a4073d37a1f3cf398e3ef147ae5e8c87cde4c86d21b"
 
-  url "https://s3.bitfocus.io/builds/companion/companion-mac-#{arch}-#{version}.dmg"
-  name "Companion"
-  desc "Enables the Elgato Streamdeck to be a professional shotbox surface"
-  homepage "https://bitfocus.io/companion"
+    url "https://s3.bitfocus.io/builds/companion/companion-mac-#{arch}-#{version}.dmg"
+    name "Companion"
+    desc "Enables the Elgato Streamdeck to be a professional shotbox surface"
+    homepage "https://bitfocus.io/companion"
 
-  app "Companion.app"
+    app "Companion.app"
 
-  uninstall_postflight do
-    system_command "pkill", args: ["-f", "Companion.app"]
+    uninstall_postflight do
+      system_command "pkill", args: ["-f", "Companion.app"]
+    end
+
+    uninstall quit: "test-companion.bitfocus.no"
+
+    zap trash: [
+      "~/Library/Application Support/companion",
+      "~/Library/Application Support/companion-launcher",
+      "~/Library/Preferences/companion.bitfocus.no.plist",
+      "~/Library/Preferences/test-companion.bitfocus.no.plist",
+    ]
   end
-
-  uninstall quit: "test-companion.bitfocus.no"
-
-  zap trash: [
-    "~/Library/Application Support/companion",
-    "~/Library/Application Support/companion-launcher",
-    "~/Library/Preferences/companion.bitfocus.no.plist",
-    "~/Library/Preferences/test-companion.bitfocus.no.plist",
-  ]
 end
