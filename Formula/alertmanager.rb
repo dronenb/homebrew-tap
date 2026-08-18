4d0ef856723f1d82d751b160e24736ed7e7e9477
Ben Dronen <dronenb@users.noreply.github.com>
2026-08-18 13:13:17 -0400

    chore(alertmanager): update to 0.34.0

--- Formula/alertmanager.rb
+++ Formula/alertmanager.rb
@@ -1,11 +1,10 @@
 class Alertmanager < Formula
   desc "Prometheus Alertmanager"
   homepage "https://prometheus.io/docs/alerting/latest/alertmanager/"
-  url "https://github.com/prometheus/alertmanager/archive/refs/tags/v0.33.1.tar.gz"
-  sha256 "dfe372ecee0704e59e166a6d72f11a689d6b8756366696a0af9fdf801059129b"
+  url "https://github.com/prometheus/alertmanager/archive/refs/tags/v0.34.0.tar.gz"
+  sha256 "f09117dd6ad62d3f53d2618ba3950ced4a318110b95ce5afe76b7df0ba74000f"
   license "Apache-2.0"
 
-  depends_on "elm" => :build
   depends_on "go" => :build
   depends_on "node" => :build
 
@@ -13,18 +12,13 @@ class Alertmanager < Formula
     ldflags = %W[
       -s -w
       -X github.com/prometheus/common/version.Version=#{version}
-      -X github.com/prometheus/common/version.Revision=2c8da51e03f3dbbed24f9711ca2d76aab4eef9c5
+      -X github.com/prometheus/common/version.Revision=085f0ef7eb41da24cab8cd000f1345b6250f2edb
       -X github.com/prometheus/common/version.Branch=HEAD
       -X github.com/prometheus/common/version.BuildUser=homebrew
       -X github.com/prometheus/common/version.BuildDate=19700101-00:00:00
     ]
 
-    cd "ui/app" do
-      inreplace "elm.json", '"elm-version": "0.19.1"', '"elm-version": "0.19.2"'
-      system "npm", "ci", "--ignore-scripts"
-      ln_sf formula_opt_bin("elm")/"elm", "node_modules/.bin/elm"
-      system "npm", "run", "build"
-    end
+    system "make", "ui-elm"
 
     system "go", "build", *std_go_args(output: bin/"alertmanager", ldflags:), "./cmd/alertmanager"
     system "go", "build", *std_go_args(output: bin/"amtool", ldflags:), "./cmd/amtool"
